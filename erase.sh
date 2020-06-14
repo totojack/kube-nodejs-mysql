1d1808ebf62fe310a2711fd3cb307691f6676be4
Luca Iovio <luca.iovio@gmail.com>
2020-06-14 16:56:43 +0200

    minor fix on bash scripts

--- erase.sh
+++ erase.sh
@@ -7,6 +7,7 @@ helm delete myappchart
 echo "deleting kube secrets"
 kubectl delete secret db-passwords
 echo "deleting docker image"
+eval $(minikube docker-env)
 docker rmi node-web-app:1.0.0 
 
 echo "helm charts"
--- setup.sh
+++ setup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-echo "enabling minikube docker env"
+echo "##################enabling minikube docker env"
 eval $(minikube docker-env)
-echo "building node js app"
+
+echo "##################building node js app"
 cd nodejsapp
 docker build -t node-web-app:1.0.0 .
 
-echo "##################creating secrets"
+echo "##################creating kube secrets"
 cd ..
 echo -n "mypwd" > mysql-password
 echo -n "mysupersecret" > mysql-root-password
@@ -16,12 +17,8 @@ rm mysql-password mysql-root-password
 echo "##################installing db manifest"
 helm install -f env/prod/db.yaml db stable/mysql
 
-sleep 5
-
 echo "##################installing node app manifest"
 helm install -f env/prod/app.yaml myappchart ./appchart
-
-sleep 5
 
 helm list
 
